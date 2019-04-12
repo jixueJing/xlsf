7037f5261f09360d457da1a14e5f54afed7e713e
jingguoliang <jingguoliang@jiuqi.com.cn>
2019-04-12 17:48:42 +0800

    小龙山房公司网站后台

---
+++ xlsf_website_manage/src/main/java/com/xlsf/website/manage/XlsfWebsiteManageApplication.java
@@ -0,0 +1,13 @@
+package com.xlsf.website.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XlsfWebsiteManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XlsfWebsiteManageApplication.class, args);
+    }
+
+}
